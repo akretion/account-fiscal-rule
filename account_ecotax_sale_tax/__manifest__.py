9bcef59fc3df978205157b9f31407253c46ae782
Florian da Costa <florian.dacosta@akretion.com>
2024-11-19 12:03:08 +0100

    [REF] Split account_ecotax_sale into account_ecotax_sale and account_ecotax_sale_tax

--- account_ecotax_sale_tax/__manifest__.py
+++ account_ecotax_sale_tax/__manifest__.py
@@ -2,18 +2,16 @@
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "sale Ecotax Management",
-    "summary": "Sale Ecotaxe",
+    "name": "sale Ecotax Management (as a tax)",
+    "summary": "Sale Ecotaxe managed as a tax",
     "version": "16.0.2.0.0",
-    "maintainers": ["mourad-ehm", "florian-dacosta"],
     "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["mourad-ehm", "florian-dacosta"],
     "website": "https://github.com/OCA/account-fiscal-rule",
     "category": "Localization/Account Taxes",
     "license": "AGPL-3",
-    "depends": ["account_ecotax", "sale"],
-    "data": [
-        "views/sale_view.xml",
-        "security/ir.model.access.csv",
-    ],
+    "depends": ["account_ecotax_sale", "account_ecotax_tax"],
+    "data": ["views/sale_view.xml"],
     "installable": True,
+    "auto_install": True,
 }
